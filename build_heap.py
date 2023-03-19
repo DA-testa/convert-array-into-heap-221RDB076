f55c84702e40a49a1c40dd6dfb5cad380f3f212e
221RDB076 <125807862+221RDB076@users.noreply.github.com>
2023-03-19 19:47:20 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -1,30 +1,29 @@
-def heapify(arr, n, i, swaps):
-    smallest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-
-    if left < n and arr[left] < arr[smallest]:
-        smallest = left
-
-    if right < n and arr[right] < arr[smallest]:
-        smallest = right
-
-    if smallest != i:
-        arr[i], arr[smallest] = arr[smallest], arr[i]
-        swaps.append((i, smallest))
-        heapify(arr, n, smallest, swaps)
+#python3
+def sift_down(i, arr, swaps):
+    min_index = i
+    left_child = 2*i + 1
+    right_child = 2*i + 2
+    n = len(arr)
+    if left_child < n and arr[left_child] < arr[min_index]:
+        min_index = left_child
+    if right_child < n and arr[right_child] < arr[min_index]:
+        min_index = right_child
+    if i != min_index:
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        swaps.append((i, min_index))
+        sift_down(min_index, arr, swaps)
 
 def build_heap(arr):
     n = len(arr)
     swaps = []
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i, swaps)
+    for i in range(n // 2, -1, -1):
+        sift_down(i, arr, swaps)
     return swaps
 
-n = int(input())
-arr = list(map(int, input().split()))
-
-swaps = build_heap(arr)
-print(len(swaps))
-for i, j in swaps:
-    print(i, j)
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    swaps = build_heap(arr)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
